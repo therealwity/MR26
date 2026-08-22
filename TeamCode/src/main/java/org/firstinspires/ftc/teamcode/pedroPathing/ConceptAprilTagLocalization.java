b02ee205bbbea7ceae352a7c31092bd6905c0592
tomica09 <tomica09@naver.com>
2026-08-22 11:55:33 +0900

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/ConceptAprilTagLocalization.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/ConceptAprilTagLocalization.java
@@ -65,7 +65,7 @@ import java.util.List;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
  */
 @TeleOp
-//@Disabled test22
+//@Disabled test223
 public class ConceptAprilTagLocalization extends LinearOpMode {
 
 
